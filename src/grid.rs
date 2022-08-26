50f7833888e32bbdc1166ddcd4196b4c4aeac99a
Nishinoyama <begoingto1414@gmail.com>
2022-08-26 16:44:51 +0900

    change: grid is wrapped

--- src/grid.rs
+++ src/grid.rs
@@ -1,9 +1,10 @@
-use std::ops::{Add, Neg, Sub};
+use core::num::Wrapping;
+use core::ops::{Add, Neg, Sub};
 
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Debug)]
 pub struct Point {
-    x: usize,
-    y: usize,
+    x: Wrapping<usize>,
+    y: Wrapping<usize>,
 }
 
 impl Add for Point {
@@ -23,10 +24,14 @@ impl Add<Direction> for Point {
     fn add(self, rhs: Direction) -> Self::Output {
         use Direction::*;
         match rhs {
-            Up => Point::from((self.x - 1, self.y)),
-            Down => Point::from((self.x + 1, self.y)),
-            Left => Point::from((self.x, self.y - 1)),
-            Right => Point::from((self.x, self.y + 1)),
+            Down => Point { x: self.x + Wrapping(1), y: self.y },
+            Right => Point { x: self.x, y: self.y + Wrapping(1) },
+            Up => Point { x: self.x - Wrapping(1), y: self.y },
+            Left => Point { x: self.x, y: self.y - Wrapping(1) },
+            UpLeft => self + Up + Left,
+            UpRight => self + Up + Right,
+            DownLeft => self + Down + Left,
+            DownRight => self + Down + Right,
         }
     }
 }
@@ -52,6 +57,7 @@ impl Sub<Direction> for Point {
 
 impl From<(usize, usize)> for Point {
     fn from((x, y): (usize, usize)) -> Self {
+        let (x, y) = (Wrapping(x), Wrapping(y));
         Point { x, y }
     }
 }
@@ -62,6 +68,10 @@ pub enum Direction {
     Down,
     Left,
     Right,
+    UpLeft,
+    UpRight,
+    DownLeft,
+    DownRight,
 }
 
 impl Neg for Direction {
@@ -73,27 +83,42 @@ impl Neg for Direction {
             Down => Up,
             Left => Right,
             Right => Left,
+            UpLeft => DownRight,
+            UpRight => DownLeft,
+            DownLeft => UpRight,
+            DownRight => UpLeft,
         }
+    }
+}
+
+impl Add for Direction {
+    type Output = Point;
+    fn add(self, rhs: Self) -> Self::Output {
+        Point::from((0, 0)) + self + rhs
     }
 }
 
 #[cfg(test)]
 mod test {
-    use std::ops::Add;
+    use core::ops::Add;
     use crate::grid::Direction::*;
     use crate::grid::Point;
 
     #[test]
     fn test() {
-        let x = [Down; 255].into_iter().fold(Point::from((0, 10)), Point::add);
-        let y = [Right; 255].into_iter().fold(Point::from((300, 20)), Point::add);
-        assert_eq!(x, Point::from((255, 10)));
-        assert_eq!(y, Point::from((300, 275)));
-        assert_eq!(x + y, Point::from((555, 285)));
-        assert_eq!(y - x, Point::from((45, 265)));
-        assert_eq!(x + Up, Point::from((254, 10)));
-        assert_eq!(x - Up, Point::from((256, 10)));
-        assert_eq!(x + Left, Point::from((255, 9)));
-        assert_eq!(x - Left, Point::from((255, 11)));
+        let x = [Down; 20].into_iter().fold(Point::from((0, 10)), Point::add);
+        let y = [Right; 20].into_iter().fold(Point::from((100, 20)), Point::add);
+        assert_eq!(x, Point::from((20, 10)));
+        assert_eq!(y, Point::from((100, 40)));
+        assert_eq!(x + y, Point::from((120, 50)));
+        assert_eq!(y - x, Point::from((80, 30)));
+        assert_eq!(x + Up, Point::from((19, 10)));
+        assert_eq!(x - Up, Point::from((21, 10)));
+        assert_eq!(x + Left, Point::from((20, 9)));
+        assert_eq!(x - Left, Point::from((20, 11)));
+        assert_eq!(x + UpLeft, Point::from((19, 9)));
+        assert_eq!(x + UpRight, Point::from((19, 11)));
+        assert_eq!(x + DownLeft, Point::from((21, 9)));
+        assert_eq!(x + DownRight, Point::from((21, 11)));
     }
 }
